fc2ff5a8ea79fb26f99bcacfa118b2e217e151a8
elenah-in6k <elenah@interlink-ua.com>
2017-07-03 22:34:29 +0300

    change date structure

--- src/main/java/com/crossword/CrosswordInfo.java
+++ src/main/java/com/crossword/CrosswordInfo.java
@@ -1,15 +1,18 @@
 package com.crossword;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
 public class CrosswordInfo {
-    private @Getter @Setter List<Integer> horizontalPosition;
+    private @Getter @Setter Integer firstSymbolPosition;
 
-    private @Getter @Setter List<Integer> verticalPosition;
+    private @Getter @Setter Integer lastSymbolPosition;
+
+    private @Getter @Setter Integer wordPosition;
+
+    private @Getter @Setter String layout;
 
     private @Getter @Setter String question;
 
