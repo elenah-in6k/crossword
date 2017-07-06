9c3e14c21658f8c5e8b73b32f200abebf0a4595f
alexandrm <oleksandrm@Interlink-ua.com>
2017-07-06 22:20:55 +0300

    1-to-1 code review result

--- src/main/java/com/crossword/CrosswordRepository.java
+++ src/main/java/com/crossword/CrosswordRepository.java
@@ -1,10 +1,14 @@
 package com.crossword;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(collectionResourceRel = "сrossword", path = "сrossword")
+@RepositoryRestResource(collectionResourceRel = "crossword", path = "crossword")
 public interface CrosswordRepository extends MongoRepository<Crossword, String> {
-
-
+    @RestResource
+    Page<Crossword> getByTestField(@Param("value") String testField, Pageable pageable);
 }
